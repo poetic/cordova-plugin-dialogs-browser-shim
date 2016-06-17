0388d71bf5a65f8f60d075531440a0842666a1fd
Chun Yang <yang2007chun@gmail.com>
2016-06-17 14:14:47 -0500

    implement API

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-if (!window) {
-  return
-}
+function () {
+  // We do not anything if its already cordova
+  if (window.cordova) {
+    return
+  }
+
+  if (window.navigator.notification) {
+    return
+  }
+
+  window.navigator.notification = {
+    alert: function(message, callback) {
+      callback(window.alert(message))
+    },
+    confirm: function (message, callback) {
+      callback(window.confirm(message))
+    },
+    prompt: function (message, callback) {
+      callback(window.prompt(message))
+    },
+    beep: function (times) {
+      // there is no equivalent function in the browser
+    }
+  }
+}()
